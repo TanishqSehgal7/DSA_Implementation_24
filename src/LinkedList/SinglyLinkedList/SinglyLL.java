e1b37123b2bd72b54f6b8aa771e26027ff288532
TanishqSehgal7 <tsgl7246@gmail.com>
2024-07-02 00:27:20 +0530

    Initial commit

---
+++ src/LinkedList/SinglyLinkedList/SinglyLL.java
@@ -0,0 +1,153 @@
+package LinkedList.SinglyLinkedList;
+
+public class SinglyLL {
+	
+	static Node head = null;
+	
+	public static class Node {
+		int data;
+		Node next;
+		public Node(int data) {
+			this.data = data;
+			this.next = null;
+		}
+	}
+	
+	public static void insertAtHead(int data) {
+		Node newNode = new Node(data);
+		newNode.next = head;
+		head = newNode;
+	}
+	
+	public static void printSLL() {
+		Node temp = head;
+		while(temp!=null) {
+			System.out.print(temp.data + " -> ");
+			temp = temp.next;
+		}
+		if(temp == null) {
+			System.out.print("NULL");
+		}
+	}
+	
+	public static void insertAtEnd(int data) {
+		Node newNode = new Node(data);
+		Node temp = head;
+		while(temp.next!=null) {
+			temp = temp.next;
+		}
+		if(temp.next == null) {
+			temp.next = newNode;
+			newNode.next = null;
+		}
+	}
+	
+	public static int getLLSize() {
+		int length = 0;
+		Node temp = head;
+		while(temp!=null) {
+			temp = temp.next;
+			length++;
+		}
+		return length;
+	}
+	
+	public static void insertAtKthPos(int data, int pos) {
+		if(head == null || pos == 1) {
+			insertAtHead(data);
+		}
+		if(pos >= getLLSize()) {
+			insertAtEnd(data);
+		}
+		int count = 1;
+		Node temp = head;
+		Node newNode = new Node(data);
+		while(count < pos-1) {
+			temp = temp.next;
+			count++;
+		}
+		Node next = null;
+		if(count == pos-1) {
+			next = temp.next;
+			temp.next = newNode;
+			newNode.next = next;
+		}
+	}
+	
+	public static void deleteAtHead() {
+		Node temp = head.next;
+		head.next = null;
+		head = temp;
+	}
+	
+	public static void deleteAtEnd() {
+		Node temp = head;
+		while(temp.next.next!=null) {
+			temp = temp.next;
+		}
+		if(temp.next.next == null) {
+			temp.next = null;
+		}
+	}
+	
+	public static void deleteAtKthPos(int pos) {
+		
+		int len = getLLSize();
+		
+		if(pos == 1) {
+			deleteAtHead();
+			return;
+		} 
+		else if(head == null) {
+			System.out.println("Can't Delete From Empty List!");
+			return;
+		}
+		else if(pos>len) {
+			deleteAtEnd();
+			return;
+		}
+		
+		Node temp = head;
+		int count = 1;
+		Node nextOfNodeToDelete = null;
+		while(count<pos-1) {
+			temp = temp.next;
+			count++;
+		}
+		nextOfNodeToDelete = temp.next.next;
+		temp.next = nextOfNodeToDelete;
+	}
+
+	public static void main(String[] args) {
+		
+		insertAtHead(10);
+		insertAtHead(20);
+		insertAtHead(30);
+		printSLL();
+		
+		insertAtEnd(5);
+		System.out.println("\n");
+		printSLL();
+		
+		insertAtKthPos(25,2);
+		System.out.println("\n");
+		printSLL();
+		
+		insertAtKthPos(15,4);
+		System.out.println("\n");
+		printSLL();
+		
+		deleteAtHead();
+		System.out.println("\n");
+		printSLL();
+		
+		deleteAtEnd();
+		System.out.println("\n");
+		printSLL();
+		
+		deleteAtKthPos(3);
+		System.out.println("\n");
+		printSLL();
+	}
+
+}
