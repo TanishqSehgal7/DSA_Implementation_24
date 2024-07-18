f6292f82fe59c181ef7a7df703dcc61e3cf5cef5
TanishqSehgal7 <tsgl7246@gmail.com>
2024-07-18 21:29:30 +0530

    Balanced Parenthesis

--- src/Stacks/StacksQuestions.java
+++ src/Stacks/StacksQuestions.java
@@ -19,8 +19,43 @@ public class StacksQuestions {
 		return reversedString;
 	}
 	
+	
+	public static boolean balancedParenthesis(String str) {
+		
+		Stack<Character> stack = new Stack<Character>();
+		
+		for(char ch: str.toCharArray()) {
+			
+			if(ch == '{' || ch == '[' || ch ==  '(') {
+				stack.push(ch);
+			} else if((ch=='}' || ch==']' || ch==')') && stack.isEmpty()) {
+				return false;
+			}
+			
+			if(!stack.isEmpty()) {
+				if(
+					(ch == '}' && stack.peek() == '{') || 
+					(ch == ']' && stack.peek() == '[') || 
+					(ch == ')' && stack.peek() == '(')) {
+					
+					stack.pop();
+				}
+			}
+		}
+		
+		if(stack.isEmpty()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
 	public static void main(String[] args) {
 		String str = "Tanishq";
 		System.out.println("Reversed String: " + reverStringUsingStack(str));
+		
+//		String parenthesis = "[()]{}{()}";
+		String parenthesis = "[(}]";
+		System.out.println(balancedParenthesis(parenthesis));
 	}
 }
