954d0bbed9d071d73153ab5bf3594a247121f896
TanishqSehgal7 <tsgl7246@gmail.com>
2024-07-28 02:04:34 +0530

    Non Repeating Character in Stream

--- src/Queue/QueueQuestions.java
+++ src/Queue/QueueQuestions.java
@@ -3,6 +3,7 @@ package Queue;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
@@ -79,6 +80,66 @@ public class QueueQuestions {
 		
 		return result;
 	}
+	
+	
+	public static void reverseKElementsOfQueue(Queue<Integer> queue, int k) {
+		
+		int n = queue.size();
+		
+		// fetch first k elements of queue and push to stack
+		Stack<Integer> stack = new Stack<Integer>();
+
+		for(int i=0;i<k;i++) {
+			int peek = queue.peek();
+			queue.remove();
+			stack.push(peek);
+		}
+		
+		// push back reversed first k elements back to queue 
+		while(!stack.isEmpty()) {
+			queue.add(stack.peek());
+			stack.pop();
+		}
+		
+		for(int i=0;i<(n-k);i++) {
+			int peek = queue.peek();
+			queue.remove();
+			queue.add(peek);
+		}
+	}
+	
+	public static String nonRepeatingCharInStream(String str) {
+		
+		HashMap<Character, Integer> map = new HashMap<Character,Integer>();
+		Queue<Character> queue = new LinkedList<Character>();
+		String ans = "";
+		
+		for(int i=0;i<str.length();i++) {
+			
+			if(map.containsKey(str.charAt(i))) {
+				map.put(str.charAt(i), map.get(str.charAt(i))+1);
+			} else {
+				map.put(str.charAt(i), 1);
+			}
+			
+			queue.add(str.charAt(i));
+			
+			while(!queue.isEmpty()) {
+				if(map.get(queue.peek())>1) { // repeating element
+					 queue.remove();
+				} else { // non repeating element
+					ans+=queue.peek();
+					break;
+				}
+			}
+			
+			if(queue.isEmpty()) {
+				ans+="#";
+			}
+		}
+		return ans;
+ 	}
+	
 
 	public static void main(String[] args) {
 		
@@ -111,6 +172,14 @@ public class QueueQuestions {
 		for(int element:firstNegativeElementList) {
 			System.out.print(element + " ");
 		}
+		
+		System.out.println("\n");
+		
+		reverseKElementsOfQueue(queue2,3);
+		System.out.println(queue2);
+		
+		String str = "aabc";
+		System.out.println(nonRepeatingCharInStream(str));
 	}
 
 }
