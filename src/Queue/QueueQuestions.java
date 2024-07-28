b0022fe9f1112fb38fe263199814767e6f10a87f
TanishqSehgal7 <tsgl7246@gmail.com>
2024-07-28 21:11:53 +0530

    Circular Tour - Array Implementation

--- src/Queue/QueueQuestions.java
+++ src/Queue/QueueQuestions.java
@@ -140,6 +140,29 @@ public class QueueQuestions {
 		return ans;
  	}
 	
+	
+	public static int circularTour(int[] petrol, int[] distance, int n) {
+		
+		int deficit = 0;
+		int balance = 0;
+		int start = 0;
+		
+		for(int i=0;i<n;i++) {
+			balance = petrol[i] - distance[i];
+			if(balance < 0) {
+				deficit+=balance;
+				start = i+1;
+				balance = 0;
+			}
+		}
+		
+		if(deficit + balance >= 0) {
+			return start;
+		} else {
+			return -1;
+		}
+	}
+	
 
 	public static void main(String[] args) {
 		
@@ -180,6 +203,11 @@ public class QueueQuestions {
 		
 		String str = "aabc";
 		System.out.println(nonRepeatingCharInStream(str));
+		
+		int[] petrol = {4,6,7,4};
+		int[] distance = {6,5,3,5};
+		System.out.println(circularTour(petrol, distance, petrol.length));
+		
 	}
 
 }
