c2b52169992457e88d022d429f5ddb8967e5198f
TanishqSehgal7 <tsgl7246@gmail.com>
2024-08-03 01:17:41 +0530

    Level Order Traversal

--- src/Trees/BinaryTree.java
+++ src/Trees/BinaryTree.java
@@ -49,7 +49,6 @@ public class BinaryTree {
 		Queue<Node> queue = new LinkedList<Node>();
 		queue.add(root);
 		
-		
 		while(!queue.isEmpty()) {
 			Node temp = queue.peek();
 			System.out.print(temp.data + " ");
@@ -66,12 +65,51 @@ public class BinaryTree {
 	}
 	
 	
-
+	public static void levelOrderTrasversalWithSeparator(Node root) {
+		
+		Queue<Node> queue = new LinkedList<Node>();
+		queue.add(root);
+		queue.add(null);
+		
+		while(!queue.isEmpty()) {
+			
+			Node temp = queue.peek();
+			queue.remove();
+			
+			// temp == null represents that a particular level has been printed
+			if(temp == null) {
+				System.out.println();
+				
+				// add separator if the queue is still not empty
+				if(!queue.isEmpty()) {
+					queue.add(null);
+				}
+				
+			} else {
+				
+				System.out.print(" " + temp.data);
+				
+				// check for left node
+				if(temp.left!=null) {
+					queue.add(temp.left);
+				} 
+				if(temp.right!=null) {
+					queue.add(temp.right);
+				}
+			}
+		}
+	}
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		root = buildTree(root);  
 		levelOrderTraversal(root);
+		
+		System.out.println("\n");
+		
+		levelOrderTrasversalWithSeparator(root);
 	}
 
 }
