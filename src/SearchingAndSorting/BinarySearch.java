05d562a567a1e05779b83ac83d87ab207556f730
TanishqSehgal7 <tsgl7246@gmail.com>
2024-09-08 20:41:55 +0530

    Search in a Rotated Sorted Array

--- src/SearchingAndSorting/BinarySearch.java
+++ src/SearchingAndSorting/BinarySearch.java
@@ -5,10 +5,8 @@ public class BinarySearch {
 	
 	public static int binarySearchSorted(int arr[], int x, int s, int e) {
 		
-		
-		while(s!=e) {
+		while(s <= e) {
 			
-			// calculate mid
 			int mid = s + (e-s)/2;
 			
 			if(arr[mid] > x) { // x exists in the left part of the array
@@ -16,23 +14,17 @@ public class BinarySearch {
 			} else if(arr[mid] < x) { // x exists in the right part of the array
 				s = mid+1;
 			}
-			
-			if(arr[mid] == x) {
-				return mid+1;
-			}
 		}
-		
 		return -1;
 	} 
 	
 	
 	public static int binarySearchReverseSorted(int arr[], int x, int s, int e) {
 		
+		// calculate mid
+		int mid = s + (e-s)/2;
 		
-		while(s!=e) {
-			
-			// calculate mid
-			int mid = s + (e-s)/2;
+		while(s <= e) {
 			
 			if(arr[mid] > x) { // x exists in the right part of the array
 				s = mid + 1;
@@ -40,21 +32,23 @@ public class BinarySearch {
 				e = mid - 1;
 			}
 			
-			if(arr[mid] == x) {
-				return mid+1;
-			}
+			mid = s + (e-s)/2;
 		}
 		
-		return -1;
+		if(arr[mid] == x) {
+			return mid;
+		} else {
+			return -1;
+		}
+		
 	}
 	
 	// single code for handling both monotonically increasing and monotonically decreasing functions
-	
 	public static int binarySearch(int arr[], int x, int s, int e) {
 		
 		boolean isIncreasing = arr[s] < arr[e];
 		
-		while(s!=e) {
+		while(s <= e) {
 			
 			int mid = s + (e-s)/2;
 			
@@ -135,26 +129,6 @@ public class BinarySearch {
 		return ans;
 	}
 	
-	
-//	public static int findPeakInMountainArray(int arr[]) {
-//		
-//		int s = 0;
-//		int e = arr.length-1;
-//		
-//		while(s<e) {
-//			
-//			int mid = s + (e-s)/2;
-//			
-//			if(arr[mid] > arr[mid+1]) {
-//				e = mid;
-//			} else if(arr[mid] < arr[mid + 1]) {
-//				s = mid + 1;
-//			}
-//		}
-//		
-//		return s;
-//	}
-	
 	public static int findPeakInMountainArray(int arr[]) {
 	    
 	    int s = 0;
@@ -174,9 +148,96 @@ public class BinarySearch {
 	    
 	    return s; // Peak index
 	}
-
 	
-
+	public static int findPivot(int arr[]) { // linear complexity [o(n)]
+		
+		int totalSum = 0;
+		
+		for(int element :  arr) {
+			totalSum += element;
+		}
+		
+		int leftSum = 0;
+		int rightSum = 0;
+		
+		// start from the first element of the array and return the index if the left and right sum are equal
+		
+		for(int i=0; i<arr.length; i++) {
+			
+			rightSum = totalSum - leftSum - arr[i];
+			
+			System.out.println("LeftSum: " + leftSum);
+			System.out.println("RightSum: " + rightSum);
+			
+			if(leftSum ==  rightSum) {
+				return i;
+			}
+			
+			leftSum += arr[i];
+		}
+		
+		return -1;
+	}
+	
+	
+	public static int findPivotInRotatedSortedArr(int arr[]) {
+		
+		int s = 0;
+		int e = arr.length-1;
+		
+		while(s<e) {
+			
+			int mid = s + (e-s)/2;
+			
+			if(arr[mid] > arr[0]) {
+				s = mid + 1;
+			} else {
+				e = mid;
+			}
+		}
+		
+		return s;
+	}
+	
+	public static int binarySearchInArr(int arr[], int x, int s, int e) {
+		
+		// calculate mid
+		int mid = s + (e-s)/2;
+		
+		while(s <= e) {
+			
+			if(arr[mid] > x) { // x exists in the left part of the array
+				e = mid-1;
+			} else if(arr[mid] < x) { // x exists in the right part of the array
+				s = mid+1;
+			}
+			
+			mid = s + (e-s)/2;
+		}
+		
+		if(x == mid) {
+			return mid;
+		} else {
+			return -1;
+		}
+	}
+	
+	public static int searchInRotatedSortedArray(int arr[], int x) {
+		
+		
+		int pivotIndex = findPivotInRotatedSortedArr(arr);
+		int ans = -1;
+		System.out.println("Pivot Index: " + pivotIndex);
+		
+		if(x >= arr[pivotIndex] && x <= arr[arr.length-1]) {
+			ans = binarySearchInArr(arr, x, pivotIndex, arr.length-1);
+		} else {
+			ans = binarySearchInArr(arr, x, 0, pivotIndex);
+		}
+		
+		return ans;
+	}
+	
 	public static void main(String[] args) {
 		
 		int arr1[] = {3,5,7,9,11,13,15,17,19,21};
@@ -200,5 +261,14 @@ public class BinarySearch {
 		int mountainArray[] = {3,5,6,4,3,2,1};
 		int peak = mountainArray[findPeakInMountainArray(mountainArray)];
 		System.out.println("Peak element is: " + peak);
+		
+		
+		int arrWithPivot[] = {1,7,3,6,5,6};
+		System.out.println(findPivot(arrWithPivot));
+		
+		int pivotInRotatedSortedArr[] = {1};
+		System.out.println("Pivot in the Rotated Sorted Array is: " + pivotInRotatedSortedArr[findPivotInRotatedSortedArr(pivotInRotatedSortedArr)]);
+		int key = 0;
+		System.out.println(key + " is found at index: " + searchInRotatedSortedArray(pivotInRotatedSortedArr,key));
 	}
 }
