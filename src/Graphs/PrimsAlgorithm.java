e14be30f52e8588a6f8f5c576b1cf84ab1b5de23
TanishqSehgal7 <tsgl7246@gmail.com>
2024-11-23 21:27:44 +0530

    Prim's Algorithm

--- src/Graphs/PrimsAlgorithm.java
+++ src/Graphs/PrimsAlgorithm.java
@@ -4,118 +4,120 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
-import java.util.Map.Entry;
 
 
 public class PrimsAlgorithm {
 
-	static Scanner scn = new Scanner(System.in);
-	static HashMap<Integer, List<Pair>> adjList = new HashMap<>();
-	static int numberOfNodes = scn.nextInt();
-	static int numberOfEdges = scn.nextInt();
-	static int[] parent = new int[numberOfNodes];
-	static int[] key = new int[numberOfNodes];
-	static boolean[] mst = new boolean[numberOfNodes];
+	static Scanner scn = new Scanner(System.in); 
+    static HashMap<Integer, List<Pair>> adjList = new HashMap<>();
+    static int numberOfNodes;
+    static int numberOfEdges;
+    static int[] parent;
+    static int[] key;
+    static boolean[] mst;
 
-	public static class Pair implements Comparable<Pair> {
-		int node;
-		int weight;
+    public static class Pair implements Comparable<Pair> {
+        int node;
+        int weight;
 
-		public Pair(int node, int weight) {
-			this.node = node;
-			this.weight = weight;
-		}
+        public Pair(int node, int weight) {
+            this.node = node;
+            this.weight = weight;
+        }
 
-		@Override
-		public int compareTo(Pair other) {
-			return Integer.compare(this.weight, other.weight); // Sort by weight (distance)
-		}
-	}
+        @Override
+        public int compareTo(Pair other) {
+            return Integer.compare(this.weight, other.weight); // Sort by weight (distance)
+        }
+    }
 
-	public static void prepareAdjList(int u, int v, int weight) {
-		Pair nodeWithWeight1 = new Pair(v, weight);
-		Pair nodeWithWeight2 = new Pair(u, weight);
-		adjList.computeIfAbsent(u, k -> new ArrayList<>()).add(nodeWithWeight1);
-		adjList.computeIfAbsent(v, k -> new ArrayList<>()).add(nodeWithWeight2);
-	}
+    public static void prepareAdjList(int u, int v, int weight) {
+        Pair nodeWithWeight1 = new Pair(v, weight);
+        Pair nodeWithWeight2 = new Pair(u, weight);
+        adjList.computeIfAbsent(u, k -> new ArrayList<>()).add(nodeWithWeight1);
+        adjList.computeIfAbsent(v, k -> new ArrayList<>()).add(nodeWithWeight2);
+    }
 
-	public static int[] primsAlgo(int key[], int parent[], boolean mst[]) {
-		
-		for (int count = 0; count < numberOfNodes; count++) {
-			
-			// Find the minimum key value among non-MST nodes
-			int mini = Integer.MAX_VALUE;
-			int u = -1;
+    public static int[] primsAlgo(int key[], int parent[], boolean mst[]) {
 
-			for (int i = 0; i < numberOfNodes; i++) {
-				if (!mst[i] && key[i] < mini) {
-					mini = key[i];
-					u = i;
-				}
-			}
+        for (int count = 0; count < numberOfNodes; count++) {
 
-			if (u == -1) {
-				System.out.println("Graph is disconnected.");
-				return parent;
-			}
+            // Find the minimum key value among non-MST nodes
+            int mini = Integer.MAX_VALUE;
+            int u = 0;
 
-			System.out.println("Selected minimum node: " + u + " with key value: " + mini);
+            for (int i = 0; i < numberOfNodes; i++) {
+                if (!mst[i] && key[i] < mini) {
+                    mini = key[i];
+                    u = i;
+                }
+            }
 
-			// Mark the selected node as part of MST
-			mst[u] = true;
+            System.out.println("Selected minimum node: " + u + " with key value: " + mini);
 
-			// Update key values for the neighbors
-			for (Pair neighbour : adjList.getOrDefault(u, new ArrayList<>())) {
-				
-				int node = neighbour.node;
-				int weight = neighbour.weight;
+            // Mark the selected node as part of MST
+            mst[u] = true;
 
-				if (!mst[node] && weight < key[node]) {
-					System.out.println("Updating node: " + node + " with parent: " + u + " and weight: " + weight);
-					parent[node] = u;
-					key[node] = weight;
-				}
-			}
-		}
-		
-		return parent;
-	}
+            // Update key values for the neighbors
+            for (Pair neighbour : adjList.getOrDefault(u, new ArrayList<>())) {
 
-	public static void main(String[] args) {
+                int node = neighbour.node;
+                int weight = neighbour.weight;
 
-		for (int i = 0; i < numberOfNodes; i++) {
-			parent[i] = -1;
-			mst[i] = false;
-			key[i] = Integer.MAX_VALUE;
-		}
+                if (!mst[node] && weight < key[node]) {
+                    System.out.println("Updating node: " + node + " with parent: " + u + " and weight: " + weight);
+                    parent[node] = u;
+                    key[node] = weight;
+                }
+            }
+        }
+        return parent;
+    }
 
-		System.out.println("Enter the value of source:");
-		int source = scn.nextInt();
-		key[source] = 0;
-		mst[source] = true;
+    public static void main(String[] args) {
+    	
+    	System.out.println("Enter Number Of Nodes:\n");
+    	numberOfNodes = scn.nextInt();
+    	System.out.println("Enter Number of Edges:\n");
+    	numberOfEdges = scn.nextInt();
+    	
+    	parent = new int[numberOfNodes];
+    	key = new int[numberOfNodes];
+    	mst = new boolean[numberOfNodes];
 
-		for (int i = 0; i < numberOfEdges; i++) {
+        for (int i = 0; i < numberOfNodes; i++) {
+            parent[i] = -1;
+            mst[i] = false;
+            key[i] = Integer.MAX_VALUE;
+        }
 
-			int u = scn.nextInt();
-			int v = scn.nextInt();
-			int wt = scn.nextInt();
-			prepareAdjList(u, v, wt);
-		}
+        System.out.println("Enter the value of source:");
+        int source = scn.nextInt();
+        key[source] = 0;
 
-		for (Entry<Integer, List<Pair>> entry : adjList.entrySet()) {
-			System.out.println("Printing values for key " + entry.getKey());
-			for (Pair pair : entry.getValue()) {
-				System.out.println(pair.node + " has weight " + pair.weight);
-			}
-		}
+        for (int i = 0; i < numberOfEdges; i++) {
 
-		parent = primsAlgo(key, parent, mst);
+            int u = scn.nextInt();
+            int v = scn.nextInt();
+            int wt = scn.nextInt();
+            prepareAdjList(u, v, wt);
+        }
+        
 
-		System.out.println("Parent array:");
-		for (int i = 0; i < parent.length; i++) {
-			System.out.println(i + " has parent " + parent[i]);
-		}                                                                        
-	}
+        for (Entry<Integer, List<Pair>> entry : adjList.entrySet()) {
+            System.out.println("Printing values for key " + entry.getKey());
+            for (Pair pair : entry.getValue()) {
+                System.out.println(pair.node + " has weight " + pair.weight);
+            }
+        }
+
+        parent = primsAlgo(key, parent, mst);
+
+        System.out.println("Parent array:");
+        for (int i = 0; i < parent.length; i++) {
+            System.out.println(i + " has parent " + parent[i]);
+        }
+    }
 }
 
 
