50287ff447de4243947c41301d2eee6f2e5838e1
TanishqSehgal7 <tsgl7246@gmail.com>
2024-12-11 00:00:35 +0530

    Articulation Points

--- src/Graphs/ArticulationPoints.java
+++ src/Graphs/ArticulationPoints.java
@@ -8,12 +8,12 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
 
-public class BridgesInGrapg_TarjansAlgo {
-	
+public class ArticulationPoints {
+
 	static HashMap<Integer, Set<Integer>> adjList = new HashMap<>();
 	static HashMap<Integer,Boolean> visited = new HashMap<>();
 	static Scanner scn = new Scanner(System.in);
-	static List<String> ans = new ArrayList<>();
+	static List<Integer> ans = new ArrayList<>();
 	static int time = 0;
 	
 	public static void prepareAdjacencyList(int u, int v) {	
@@ -21,45 +21,44 @@ public class BridgesInGrapg_TarjansAlgo {
 		adjList.computeIfAbsent(v, k-> new HashSet<>()).add(u);
 	}
 	
-	public static List<String> findBridges(int u, int discovery[], int low[], int parent[], boolean visited[]) {
+	public static List<Integer> findArticulationPoints(int u, int discovery[], int low[], int parent[], boolean visited[]) {
 		
-		// update discovery and low with incremented time
 		discovery[u] = low[u] = ++time;
-		
 		visited[u] = true;
 		
-		// process neighbors of u
+		int childCount = 0;
 		
 		for(int neighbor: adjList.getOrDefault(u, new HashSet<>())) {
 			
-			// node is not visited
 			if(!visited[neighbor]) {
 				
+				childCount++;
 				parent[neighbor] = u;
 				visited[neighbor] = true;
 				
-				// make dfs call
-				findBridges(neighbor, discovery, low, parent, visited);
+				findArticulationPoints(neighbor, discovery, low, parent, visited);
 				
 				low[u] = Math.min(low[u], low[neighbor]);
 				
-				// check if u, neighbor is a bridge
-				if(low[neighbor] > discovery[u]) {
-					ans.add(u + " - " + neighbor);
+				// check condition for articulation point for a non rooted node
+				if(parent[u]!=-1 && low[neighbor] >= discovery[u]) {
+					ans.add(u);
 				}
 				
-			} else if(neighbor != parent[u]) { 
-				// back edge scenario and the node is visited
+			} else if(neighbor != parent[u]) { // back edge
 				low[u] = Math.min(low[u], discovery[neighbor]);
 			}
+		}
+		
+		if(parent[u] == -1 && childCount > 1) { // rooted node
+			ans.add(u);
 		}
 		
 		return ans;
 	}
 	
-
 	public static void main(String[] args) {
-
+		
 		System.out.println("Enter the number of nodes");
 		int numberOfNodes = scn.nextInt();
 		
@@ -84,11 +83,10 @@ public class BridgesInGrapg_TarjansAlgo {
 		
 		for(int i=0; i<numberOfNodes; i++) {
 			if(!visited[i]) {
-				ans = findBridges(i, dicovery, low, parent, visited);
+				ans = findArticulationPoints(i, dicovery, low, parent, visited);
 			}
 		}
 		
 		System.out.println(ans);
 	}
-
 }
